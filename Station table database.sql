a858ee3dc13eb3ea36654e9cf7e5589a2ff6fd90
Gruha-sridevi <52320079+Gruha-sridevi@users.noreply.github.com>
2022-04-22 10:42:22 +0530

    Add files via upload

---
+++ Station table database.sql
@@ -0,0 +1,13 @@
+
+create database wileyproject;
+use wileyproject;
+create table station(
+station_id int primary key,
+station_name varchar(20));
+
+insert into station
+values(1,'delhi'),
+(2,'kolkata'),
+(3,'mumbai'),
+(4,'noida'),
+(5,'hyderabad');
