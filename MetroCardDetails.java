efaefb2b31bfdaaa01cf9651890b0f1c61937baf
Ayyappa Reddy <102216971+ayyappareddy12@users.noreply.github.com>
2022-05-09 09:31:08 +0530

    Add files via upload

---
+++ MetroCardDetails.java
@@ -0,0 +1,33 @@
+package entity;
+
+public class MetroCardDetails {
+	public String toString() {
+		return "MetroCardDetails [metroId=" + metroId + ", startValidity=" + startValidity + ", endValidity="
+				+ endValidity + ", metroBalance=" + metroBalance + "]";
+	}
+	private int metroId;
+	private String startValidity;
+	private String endValidity;
+	private int metroBalance;
+	public MetroCardDetails(int metroId, String startValidity, String endValidity, int metroBalance) {
+		super();
+		this.metroId = metroId;
+		this.startValidity = startValidity;
+		this.endValidity = endValidity;
+		this.metroBalance = metroBalance;
+	}
+	public int getMetroId() {
+		return metroId;
+	}
+	public String getStartValidity() {
+		return startValidity;
+	}
+	public String getEndValidity() {
+		return endValidity;
+	}
+	public int getMetroBalance() {
+		return metroBalance;
+	}
+	
+
+}
